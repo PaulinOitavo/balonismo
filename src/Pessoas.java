0b593891ffc3c80badd19465b7fbc69c495514ea
PaulinOitavo <henriquent90@gmail.com>
2023-06-01 12:25:59 -0300

    tentativa incompleta

---
+++ src/Pessoas.java
@@ -0,0 +1,24 @@
+public class Pessoas {
+
+    public class Pessoa {
+         String nome;
+         String CPF;
+         String RG;
+         String telefone;
+         String telefoneContatoEmergencias;
+         String endereco;
+    }
+
+    public class Piloto {
+         String nome;
+         String CPF;
+         String numeroRegistroANAC;
+    }
+
+    public class Balao {
+         String numeroIdentificacao;
+         String registroRegularidade;
+    }
+
+
+}
